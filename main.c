e2af3aefe30514f33ea014f7c0748f2a844ea38a
johanw31 <49872974+johanw31@users.noreply.github.com>
2019-09-10 22:10:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,16 @@
 #include <math.h>
 
 int main (){
+    char copy[5];
+    char *lol = "Hallo";
 
-
-	printf ("%d\n", strsearch ("hallo","ahaaaahaaaaahallo"));
-
+	printf ("%d\n", strsearch ("hallo","ahaaaahaaaaahallooggg"));
+	printf ("%d\n", ggt(126,4236));
+	strcopy ("Hallo",copy);
+	printstr(copy);
 
 	return 0;
 }
+// https://youtu.be/B_MFZbny4vU?list=RDB_MFZbny4vU&t=82    
 
 
