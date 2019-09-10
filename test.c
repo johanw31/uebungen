4077e1c1b9cb6c6b110d56d7f41d1ef7d4cdfe19
johanw31 <49872974+johanw31@users.noreply.github.com>
2019-09-10 22:08:06 +0200

    Update test.c

--- test.c
+++ test.c
@@ -2,69 +2,120 @@
 #include <stdio.h>
 #include "test.h"
 
-
-double sqrt (double x){
+double sqrt(double x) {
 	double a = x;
-	for (int i=0;i<5;i++){
-		x = x-((x*x-a)/(2*x));
+	for (int i = 0; i < 5; i++) {
+		x = x - ((x * x - a) / (2 * x));
 	}
-return x;
+	return x;
 }
 
-
-double iterate (double x_0, double eps, unsigned int nmax){
-	double delta, relDelta, x_k_1,x_k = x_0;
+double iterate(double x_0, double eps, unsigned int nmax) {
+	double delta, relDelta, x_k_1, x_k = x_0;
 	unsigned int k = 0;
 
 	do {
 		x_k_1 = x_k;
-		x_k  = x_k-((x_k*x_k-x_0)/(2*x_k));;
-		delta = dabs ((x_k_1 - x_k));
-		relDelta = (100/x_0) * delta ;
-		printf ("debug\n");
+		x_k = x_k - ((x_k * x_k - x_0) / (2 * x_k));
+		;
+		delta = dabs((x_k_1 - x_k));
+		relDelta = (100 / x_0) * delta;
+		printf("debug\n");
 
-	}while (delta > eps && ++k < nmax);
+	} while (delta > eps && ++k < nmax);
 
 	return x_k;
 }
 
-
-
-double dmasch (double x){
-	while ((x+1)-1 != 0){
-		x=x/2;
+double dmasch(double x) {
+	while ((x + 1) - 1 != 0) {
+		x = x / 2;
 	}
 	return x;
 
 }
 
-float fmasch (float x){
-	while ((x+1)-1 != 0){
-		x=x/2;
+float fmasch(float x) {
+	while ((x + 1) - 1 != 0) {
+		x = x / 2;
 	}
 	return x;
 
 }
 
-int strlength (char* string){
+int strlength(char *string) {
 	int counter = 0;
-	while (*string++){
+	while (*string++) {
 		counter++;
 	}
 	return counter;
 }
 
-int strsearch (char* needle, char* string){
-	int length1 = strlength (needle);
-	int length2 = strlength (string);
+int strsearch(char *needle, char *string) {
+	int length1 = strlength(needle);
+	int length2 = strlength(string);
 	int needlePos;
-	for (int i = 0; i<length2; i++){
-		if (string[i] == needle[0]){
+	for (int i = 0; i < length2; i++) {
+		if (string[i] == needle[0]) {
 			needlePos = i;
-			for (int j=0; j<length1; j++){
-				if (string[i+j] != needle[j]) needlePos = -1;
+			for (int j = 0; j < length1; j++) {
+				if (string[i + j] != needle[j])
+					needlePos = -1;
+				if (needlePos != -1 && (j == length1 - 1))
+					return needlePos;
 			}
 		}
 	}
 	return needlePos;
 }
+
+int faculty(int fakultaet) {
+	if (fakultaet == 1)
+		return 1;
+	return fakultaet * faculty(fakultaet - 1);
+}
+
+int power(int basis, int exponent) {
+	if (exponent == 1)
+		return basis;
+	return basis * power(basis, exponent - 1);
+}
+
+void swapInt(int *A, int *B) {
+	int tmp = *A;
+	*A = *B;
+	*B = tmp;
+}
+
+int ggt(int A, int B) {
+	int GGT;
+	int rest = 1;
+	if (A < B) { swapInt (&A,&B);}
+	while (rest) {
+		rest = A % B;
+		GGT = A / B;
+		A = B;
+		B = rest;
+	}
+	return A;
+
+}
+
+int kgv (int A, int B){
+	return (A / ggt(A,B))*B;
+}
+
+void strcopy (char* source, char* copy){
+	int n = strlength(source);
+	int i=0;
+	for (;i<n;i++){
+		copy[i] = source[i];
+	}
+}
+
+void printstr (char *s){
+	while (s){
+	putchar(*s++);
+	}
+}
+
